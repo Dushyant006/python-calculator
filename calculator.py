5d48fccce027425718c4c8aa9adf15eea8356e66
Dushyantcoder07 <dushyantsisodiya18@gmail.com>
2026-06-08 10:26:02 +0530

    add basic structure

---
+++ calculator.py
@@ -0,0 +1,26 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b == 0:
+        return "Cannot divide by zero"
+    return a / b
+
+while True:
+    print("\nCalculator")
+    print("1. Add")
+    print("2. Subtract")
+    print("3. Multiply")
+    print("4. Divide")
+    print("5. Exit")
+
+    choice = input("Choose: ")
+
+    if choice == "5":
+        break
