d21e6540eabdf986f0848b31701f3b3f7a2e42b9
Dushyantcoder07 <dushyantsisodiya18@gmail.com>
2026-06-10 21:16:15 +0530

    add power operation

--- calculator.py
+++ calculator.py
@@ -12,20 +12,21 @@ def divide(a, b):
         return "Cannot divide by zero"
     return a / b
 
+def power(base, exponent):
+    return base**exponent
+
 while True:
     print("\nCalculator")
     print("1. Add")
     print("2. Subtract")
     print("3. Multiply")
     print("4. Divide")
-    print("5. Exit")
+    print("5. Power")
+    print("6. Exit")
 
     choice = input("Choose: ")
 
-    if choice == "5":
-        break
-
-    elif choice == "1":
+    if choice == "1":
         try:
             user_input = [float(x) for x in input("Enter numbers with spaces: ").split()]
 
@@ -35,7 +36,8 @@ while True:
         
         print("Result:", sum(user_input))
         break
-    if choice == "2":
+
+    elif choice == "2":
         input_numbers = input("Enter numbers separated by spaces: ")
         numbers = input_numbers.split()
         if len(numbers) >= 2:
@@ -45,16 +47,26 @@ while True:
             print("Result:", result)
         else:
             print("Please enter at least two numbers for subtraction.")
+            
+    
+    elif choice == "3":
+        num1= float(input("Enter your 1st number: "))
+        num2= float(input("Enter your 2nd number: "))
+        print("Result:", multiply(num1, num2))
           
 
     elif choice == "4":
         num1= float(input("Enter your first number: "))
         num2= float(input("Enter your second number: "))
         print("Result:", divide(num1, num2))
+
+
     elif choice == "5":
+        base = float(input("Enter base: "))
+        exponent = float(input("Enter exponent: "))
+
+        print("Result:", power(base, exponent))
+    
+    
+    elif choice == "6":
         break
-        
-    elif choice == "3":
-        num1= float(input("Enter your 1st number: "))
-        num2= float(input("Enter your 2nd number: "))
-        print("Result:", multiply(num1, num2))
